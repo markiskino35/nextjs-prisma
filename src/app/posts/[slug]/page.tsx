d6fcf0d36a884b1a573d96ea08c98cd9e492fb71
Alief Mukhlis <markiskino35@gmail.com>
2025-03-18 15:31:26 +0800

    fix

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -10,15 +10,10 @@ export default async function PostPage({
       slug: params.slug,
     },
   });
-
-  if (!post) {
-    return <h1>Post not found</h1>;
-  }
-
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+      <h1>{post?.title}</h1>
+      <p>{post?.content}</p>
     </main>
   );
 }
