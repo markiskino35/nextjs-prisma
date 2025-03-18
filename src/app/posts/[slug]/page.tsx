f26982f066305ad7a8ef0eee6fabf7e176508c73
Alief Mukhlis <markiskino35@gmail.com>
2025-03-18 15:19:23 +0800

    fix bug

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,21 +1,24 @@
 import { prisma } from "@/lib/prisma";
 
-interface PostPageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export default async function PostPage({ params }: PostPageProps) {
+export default async function PostPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const post = await prisma.post.findUnique({
     where: {
       slug: params.slug,
     },
   });
+
+  if (!post) {
+    return <h1>Post not found</h1>;
+  }
+
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-      <h1>{post?.title}</h1>
-      <p>{post?.content}</p>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </main>
   );
 }
